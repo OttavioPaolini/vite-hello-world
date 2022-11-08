73b497658d335eff9d9da13c1f855da15ed125da
Ottavio8 <paolini.ottavio8@gmail.com>
2022-11-08 09:38:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+
+import AppTitle from './components/AppTitle.vue'
+
+export default{
+  data(){
+    
+  },
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  
+<AppTitle/>
+
+</template>
+
+<style scoped>
+
+  
+</style>
